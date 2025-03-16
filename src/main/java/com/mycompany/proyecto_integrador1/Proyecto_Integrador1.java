b812978e39d668cb866d5830799c822b31e0f815
sanma <sanma@SannsPC>
2025-03-16 12:00:44 -0500

    Cree el repositorio

---
+++ src/main/java/com/mycompany/proyecto_integrador1/Proyecto_Integrador1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_integrador1;
+
+/**
+ *
+ * @author sanma
+ */
+public class Proyecto_Integrador1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
