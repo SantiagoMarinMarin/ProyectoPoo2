40d068ddb8b342ccf95e81455961be0b3a9a8c31
Polasana2020 <carlosf.polanias@unac.edu.co>
2025-03-24 15:44:48 -0500

    Hago validación para lo que es colocar nombre, apellido y los otros de manera inmediata.

---
+++ src/main/java/com/mycompany/proyectopoo2/CampoObligatorioException.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.proyectopoo2;
+
+public class CampoObligatorioException extends Exception {
+
+    public CampoObligatorioException() {
+    }
+     public CampoObligatorioException(String mensaje) {
+        super(mensaje);
+    }
+}
